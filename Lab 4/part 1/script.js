6614d297eec4f391baaeb9b9521228482532e511
PalaashG0 <palaash.gajjar@dcmail.ca>
2026-04-09 15:07:52 -0400

    Update script.js
    fixed missing apostrophes and got rid of long comments at beginning

--- Lab 4/part 1/script.js
+++ Lab 4/part 1/script.js
@@ -18,18 +18,10 @@ function randomValueFromArray(array) {
 
 // Raw text strings
 
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
 const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
-// the soup kitchen
-// Disneyland
-// the White House
 const places = ["the soup kitchen", "Disneyland", "the White House"];
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
+
 const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a sluge and slithered away"];
 
   // Partial return random string function
@@ -39,7 +31,7 @@ function returnRandomStoryString() {
   const randomPlace = randomValueFromArray(places);
   const randomEvent = randomValueFromArray(events);
 
-  let storyText = It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised - ${randomCharacter} weighs 300 pounds, and it was a hot day. ;
+  let storyText = 'It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised - ${randomCharacter} weighs 300 pounds, and it was a hot day.' ;
 
   return storyText;
 }
@@ -56,8 +48,8 @@ function generateStory() {
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300 / 14)} stone ;
-    const temperature = Math.round((94 - 32) * (5 / 9))} Celsius ;
+    const weight = '{Math.round(300 / 14)} stone' ;
+    const temperature = '{Math.round((94 - 32) * (5 / 9))} Celsius' ;
     newStory = newStory.replace("300 pounds", weight);
     newStory = newStory.replace("94 Fahrenheit", temperature);
   }
